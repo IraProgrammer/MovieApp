a65f5ae16961af1343936db57d16a29bbcbbd7af
Irina Laskova <Irinalaskova@mail.ru>
2018-07-20 18:06:06 +0300

    еще не маппила в разметке не вынесла размеры в dimens.xml, т.к. пока все неточно строки пока что не выносила в strings.xml

--- app/src/main/java/com/example/irishka/movieapp/ui/film/view/DescriptionFragment.java
+++ app/src/main/java/com/example/irishka/movieapp/ui/film/view/DescriptionFragment.java
@@ -7,15 +7,24 @@ import android.telecom.TelecomManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
 import com.arellomobile.mvp.MvpFragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.example.irishka.movieapp.R;
 import com.example.irishka.movieapp.data.models.DescriptionModel;
+import com.example.irishka.movieapp.data.models.Genre;
 import com.example.irishka.movieapp.ui.film.presenter.DescriptionPresenter;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -26,6 +35,7 @@ import dagger.Lazy;
 import dagger.android.DaggerFragment;
 import dagger.android.support.AndroidSupportInjection;
 
+import static android.content.Context.DEVICE_POLICY_SERVICE;
 import static com.example.irishka.movieapp.ui.movies.view.MoviesListActivity.MOVIE_ID;
 
 public class DescriptionFragment extends MvpAppCompatFragment implements DescriptionView {
@@ -47,6 +57,27 @@ public class DescriptionFragment extends MvpAppCompatFragment implements Descrip
 
     @BindView(R.id.film_title)
     TextView filmTitle;
+
+    @BindView(R.id.year_country)
+    TextView yearCountry;
+
+    @BindView(R.id.genre)
+    TextView genre;
+
+    @BindView(R.id.duration)
+    TextView duration;
+
+    @BindView(R.id.rate)
+    TextView rate;
+
+    @BindView(R.id.film_image)
+    ImageView image;
+
+    @BindView(R.id.overview)
+    TextView overview;
+
+    @BindView(R.id.see_also)
+    TextView seeAlso;
 
     public static DescriptionFragment newInstance(){
         return new DescriptionFragment();
@@ -75,6 +106,41 @@ public class DescriptionFragment extends MvpAppCompatFragment implements Descrip
 
     @Override
     public void showDescription(DescriptionModel description) {
+
         filmTitle.setText(description.getTitle());
+
+        yearCountry.setText(description.getReleaseDate() + ", " + description.getProductionCountries().get(0).getName());
+
+        setPicture(description);
+
+        setGenre(description);
+
+        duration.setText(description.getRuntime() + ", " + description.getAdult());
+
+        rate.setText(String.valueOf(description.getPopularity()));
+
+        overview.setText(description.getOverview());
+
+        seeAlso.setText("See Also");
+    }
+
+    private void setPicture(DescriptionModel description){
+        Glide.with(this)
+                .load("http://image.tmdb.org/t/p/w500//" + description.getPosterPath())
+                .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                        .placeholder(R.drawable.no_image)
+                        .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL))
+                .into(image);
+    }
+
+    private void setGenre(DescriptionModel description){
+        String genresStr = "";
+
+        List<Genre> genres = description.getGenres();
+        for (Genre g: genres) {
+            genresStr += g.getName() + ", ";
+        }
+
+        genre.setText(genresStr);
     }
 }
