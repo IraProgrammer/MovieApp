e1944a5a94f643bcc0e812f0f1edc1c1b6148059
Irina Laskova <Irinalaskova@mail.ru>
2018-08-22 13:24:35 +0300

    .gitignore is now working

---
+++ app/src/main/java/com/example/irishka/movieapp/ui/filters/di/qualifiers/Sort.java
@@ -0,0 +1,12 @@
+package com.example.irishka.movieapp.ui.filters.di.qualifiers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Sort {
+
+}
